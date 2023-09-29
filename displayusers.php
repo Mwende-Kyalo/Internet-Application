2835fa12952650dd185b280c7133b270bc0e3c11
Mwende_Kyalo <tracey.kyalo@strathmore.edu>
2023-09-29 21:55:24 +0300

    Updated Display Users

--- displayusers.php
+++ displayusers.php
@@ -6,16 +6,22 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<table border='1'>
     <tr>
-        <th>ID</th>
-        <th>Username</th>
+        <th>First Name</th>
+        <th>Last name</th>
         <th>Email</th>
+        <th>Conact</th>
+        <th>Password</th>
+        <th>DOB</th>
     </tr>";
 
 foreach ($users as $user) {
     echo "<tr>";
-    echo "<td>" . $user['id'] . "</td>";
-    echo "<td>" . $user['username'] . "</td>";
+    echo "<td>" . $user['f_name'] . "</td>";
+    echo "<td>" . $user['l_name'] . "</td>";
     echo "<td>" . $user['email'] . "</td>";
+    echo "<td>" . $user['contact'] . "</td>";
+    echo "<td>" . $user['password'] . "</td>";
+    echo "<td>" . $user['DOB'] . "</td>";
     echo "</tr>";
 }
 
